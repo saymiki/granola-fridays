0d4b563d1037a134b6c03b8656975f384d8a09ca
Saymiki <mikaela.montesclaros@gmail.com>
2023-06-28 15:57:41 -0500

    added arrays

--- assets/script.js
+++ assets/script.js
@@ -23,13 +23,48 @@ function setTime() {
   }
 }
 
-// // Define the quiz settings
-// var quizDuration = 60; // in seconds
-// var highScoresKey = "highScores";
+// Define an array of quiz questions and answer choices
+const quizQuestions = [
+  {
+    question: "Where do you go to make a repository?",
+    choices: ["GitHub", "Club Penguin", "Facebook"],
+    correctAnswer: "GitHub",
+  },
+  {
+    question: "What does HTML stand for?",
+    choices: [
+      "Hairy Triceratops Makes Lemonade",
+      "Harry Tries Making Limeaid",
+      "HyperText Markup Language",
+    ],
+    correctAnswer: "HyperText Markup Language",
+  },
+  {
+    question: "What is the correct way to comment out lines in HTML?",
+    choices: ["<!--- --->", "// //", "?? ?? ??"],
+    correctAnswer: "<!--- --->",
+  },
+];
 
-// // Define the initial state of the quiz
-// var currentQuestionIndex = 0;
-// var score = 0;
+// Define the quiz settings
+const highScoresKey = "highScores";
+
+// Define the initial state of the quiz
+const currentQuestionIndex = 0;
+const score = 0;
+
+// Psuedocode
+// create a cycle for the arrays
+// assign css to arrays so they display properly
+// onclick of start button first array will appear
+// upon selection of answer score will increase or stay the same
+// if correct asnwer increase score
+// if  other than correct answer time will decrease
+// once answer is chosen next array will appear
+// cycle repeats until end of array
+// at end of array display score
+// allow input field for highscore input and to save
+// create try again button to redirect to start and essenstially refresh the page maybe
 
 // // Get references to the HTML elements
 // var startButton = document.getElementById("start-button");
@@ -40,26 +75,3 @@ function setTime() {
 // var timerEl = document.querySelector(".time");
 // var scoreEl = document.getElementById("score");
 // var highScoresEl = document.getElementById("high-scores");
-
-// // Define an array of quiz questions and answer choices
-// const quizQuestions = [
-//   {
-//     question: "Where do you go to make a repository?",
-//     choices: ["GitHub", "Club Penguin", "Facebook"],
-//     correctAnswer: "GitHub",
-//   },
-//   {
-//     question: "What does HTML stand for?",
-//     choices: [
-//       "Hairy Triceratops Makes Lemonade",
-//       "Harry Tries Making Limeaid",
-//       "HyperText Markup Language",
-//     ],
-//     correctAnswer: "HyperText Markup Language",
-//   },
-//   {
-//     question: "What is the correct way to comment out lines in HTML?",
-//     choices: ["<!--- --->", "// //", "?? ?? ??"],
-//     correctAnswer: "<!--- --->",
-//   },
-// ];
